de57aa99215d2f537292de9aab44c46b45169480
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-04-12 06:30:55 +0600

    create-payment-code done

--- create-payment-code.php
+++ create-payment-code.php
@@ -3,5 +3,10 @@
     $amount = $_POST['amount'] ;
     $payment_date = $_POST['payment_date'] ;
 
-    
+    //Database
+    require_once "db_connect.php";
+    $query ="INSERT INTO payment(student_id,amount,payment_date) VALUES ('$student_id','$amount','$payment_date')";
+    mysqli_query($conn,$query);
+    header("location:read_payment.php")
+?>
 ?>
