95d60e14eb551edf7e4600d9c39c3abf839e8b7e
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-04-12 06:18:33 +0600

    create payment done

--- create-payment.php
+++ create-payment.php
@@ -1,4 +1,4 @@
-<!-- createPayment( payment_id, student_id, amount, date ) -->
+<!-- Crate payment -->
 <?php
     $student_id = $_GET['id'];
 ?>
@@ -6,6 +6,11 @@
     <form action="create_payment_code.php" method="POST">
         <label>Student ID</label>  <br>
         <input name="student_id" readonly type="text" value=<?=$student_id?>> <br>
+        <label>Amount</label>  <br>
+        <input name="amount" type="text" placeholder="Enter Amount"> <br>
+        <label>Date</label> <br>
+        <input name="payment_date" type="date">  <br> <br>
+        <button type="submit">Create Payment</button>
         
     </form>
     <br>
